3fe6932437f8076d51cb1304acf1d8cd9ec0ebb7
Md Alamin Hossain <58477678+alaminjava@users.noreply.github.com>
2023-03-09 05:45:28 +0600

    Add files via upload

---
+++ Python and Gradio.py
@@ -0,0 +1,43 @@
+import gradio as gr
+import openai
+import config
+
+openai.api_key = config.OPENAI_API_KEY
+
+
+messages = [
+
+    {"role": "system", "content": "You are a helpful assistant."}
+     
+]
+
+def transcribe(audio):
+    global messages
+    
+    print(audio)
+
+    audio_file= open(audio, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    messages.append({"role": "user", "content": transcript["text"]})
+    
+    
+    response= openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    
+    system_message = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": system_message})
+    
+    chat_transcript = ""
+    for message in messages:
+        if message ['role'] != 'system':
+            chat_transcript += message['role'] + ": " + message['content'] + "\n\n"
+    
+    return chat_transcript
+
+
+ui = gr.Interface(fn=transcribe, inputs=gr.Audio(source="microphone", type="filepath"), outputs="text").launch()
+
+
+ui.launch()
